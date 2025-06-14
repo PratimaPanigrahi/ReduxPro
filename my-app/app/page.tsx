a9eb36185d6c799d51fef270cad97d0b21262280
PratimaPanigrahi <148442618+PratimaPanigrahi@users.noreply.github.com>
2025-06-14 12:49:57 +0000

    changes

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,25 @@
-import TodoList from './components/store/store';
+import MyComponent from './MyComponent';
+import './styles.css';
 
 export default function Page() {
   return (
-    <div>
-      <h1>Todo List App</h1>
-      <TodoList />
+    <div className="my-style">
+      <MyComponent />
     </div>
   );
+}
+import './styles.css';
+
+export default function Page() {
+  return <div className="my-style">Hello!</div>;
+}
+
+export default function MyComponent() {
+  return <div>This is MyComponent!</div>;
+}
+import { myFunction } from './utils';
+
+export default function Page() {
+  myFunction();
+  return <div>Check the console!</div>;
 }
